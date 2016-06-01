f74bb5d8163787bcfc01ba46694f0b503e723a24
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2016-06-01 09:29:34 +0200

    Updates messages

--- configuration.js
+++ configuration.js
@@ -3,7 +3,7 @@ var configuration = {
         {
             imagePath: "images/dummies.png",
             backgroundImagePath: "images/light-blue-opacity.png",
-            message: "Ouch, vous étiez à 2 doigts de gagner le livre de réparation de vélo pour les nuls..."
+            message: "Ouch, tu étais à 2 doigts de gagner le livre de réparation de vélo pour les nuls..."
         },
         {
             imagePath: "images/collants.png",
@@ -18,12 +18,12 @@ var configuration = {
         {
             imagePath: "images/radiateurs.png",
             backgroundImagePath: "images/light-blue-opacity.png",
-            message: "C'est dommage,<br> vous aviez presque gagné <br>un radiateur pour avoir le cul moins gelé!"
+            message: "C'est dommage,<br> tu avais presque gagné <br>un radiateur pour avoir le cul moins gelé!"
         },
         {
             imagePath: "images/cats.png",
             backgroundImagePath: "images/orange-opacity.png",
-            message: "Mince alors, vous avez perdu.<br>Cette mignonne boule de poils attendra"
+            message: "Mince alors, tu as perdu.<br>Cette mignonne boule de poils attendra.."
         },
         {
             imagePath: "images/disney.png",
@@ -31,7 +31,7 @@ var configuration = {
             class: "winner",
             message: "" +
                 "Félicitations!! " +
-                "Vous êtes l'heureuse gagnante d\'un séjour d'un weekend pour deux personnes à disneyland" +
+                "Tu es l'heureuse gagnante d\'un séjour d'un weekend pour deux personnes à disneyland" +
                 "<br> <img src='images/leo.gif'>" +
             ""
         }
